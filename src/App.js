41ade5829fa14e437f36cab6f429b8ff426f14a7
James Farfan <jamesrojas710@gmail.com>
2021-04-21 23:53:19 -0400

    setup div contents and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from '../src/Components/navbar/Navbar';
-
+//Below just testing how it renders out
+import Incubator from '../src/Pages/incubator/Incubator';
+//
+import MainContent from '../src/Components/maincontent/Maincontent';
 function App() {
   return (
     <div className='App' >
       <Navbar />
-
+      <MainContent />
+      <Incubator />
     </div>
   );
 }
